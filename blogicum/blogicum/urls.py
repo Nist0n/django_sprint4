325cf2d55dfd3f755bb01ab004bb293931defae4
Niston <vadimpavlov@bk.ru>
2025-05-17 23:15:30 +0300

    Update urls.py

--- blogicum/blogicum/urls.py
+++ blogicum/blogicum/urls.py
@@ -12,11 +12,11 @@ handler500 = 'pages.views.server_error'
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('auth/', include('django.contrib.auth.urls')),
-    path('', include('blog.urls')),
     path(
-            'auth/registration/',
-            blog_views.RegistrationView.as_view(),
-            name='registration'
-        ),
+        'auth/registration/',
+        blog_views.RegistrationView.as_view(),
+        name='registration'
+    ),
+    path('', include('blog.urls')),
     path('pages/', include('pages.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
